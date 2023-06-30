4e0fac8bdfef02aaadf2c2f4791558e7a6923e54
Philip Venter <philip.venter@roomraccoon.com>
2023-06-30 23:26:58 +0200

    eliminate magic values

--- JWT.php
+++ JWT.php
@@ -2,65 +2,58 @@
 
 class JWT
 {
-    protected const ALGORITHM = 'HS256';
-    protected const TYPE = 'JWT';
+    public const HEADER = 'header';
+    public const PAYLOAD = 'payload';
+    public const SIGNATURE = 'signature';
 
-    /**
-     * Create new JWT object
-     * @param string $secret
-     * @param string $issuer
-     * @param int $ttl
-     */
+    protected const HASH_ALGORITHM = 'SHA256';
+
+    protected const HEADER_ALGORITHM = 'alg';
+    protected const HEADER_ALGORITHM_VALUE = 'HS256';
+    protected const HEADER_TYPE = 'typ';
+    protected const HEADER_TYPE_VALUE = 'JWT';
+
+    protected const PAYLOAD_ISSUER = 'iss';
+    protected const PAYLOAD_ISSUED_AT = 'iat';
+    protected const PAYLOAD_EXPIRY = 'exp';
+    protected const PAYLOAD_NOT_BEFORE = 'nbf';
+
     public function __construct(
         protected string $secret = '',
         protected string $issuer = '',
         protected int $ttl = 3_600
     ) { }
 
-    /**
-     * Generate JWT token
-     * @param array $payload
-     * @param array $headers
-     * @return string
-     */
-    public function generate(
-        array $payload,
-        array $headers = []
-    ): string {
-        $headers['alg'] = self::ALGORITHM;
-        $headers['typ'] = self::TYPE;
+    public function generate(array $payload, array $headers = []): string
+    {
+        $headers[self::HEADER_ALGORITHM] = self::HEADER_ALGORITHM_VALUE;
+        $headers[self::HEADER_TYPE] = self::HEADER_TYPE_VALUE;
 
         if (!empty(trim($this->issuer))) {
-            $payload['iss'] = $this->issuer;
+            $payload[self::PAYLOAD_ISSUER] = $this->issuer;
         }
 
-        if (isset($payload['iat']) && is_string($payload['iat'])) {
-            $payload['iat'] = strtotime($payload['iat']);
-        } elseif (isset($payload['iat']) && is_bool($payload['iat']) && $payload['iat']) {
-            $payload['iat'] = time();
+        if (is_string($payload[self::PAYLOAD_ISSUED_AT] ?? null)) {
+            $payload[self::PAYLOAD_ISSUED_AT] = strtotime($payload[self::PAYLOAD_ISSUED_AT]);
+        } elseif (is_bool($payload[self::PAYLOAD_ISSUED_AT] ?? null) && $payload[self::PAYLOAD_ISSUED_AT]) {
+            $payload[self::PAYLOAD_ISSUED_AT] = time();
         }
 
-        if (isset($payload['exp']) && is_string($payload['exp'])) {
-            $payload['exp'] = strtotime($payload['exp']);
-        } elseif (isset($payload['exp']) && is_bool($payload['exp']) && $payload['exp']) {
-            $payload['exp'] = time() + $this->ttl;
+        if (is_string($payload[self::PAYLOAD_EXPIRY] ?? null)) {
+            $payload[self::PAYLOAD_EXPIRY] = strtotime($payload[self::PAYLOAD_EXPIRY]);
+        } elseif (is_bool($payload[self::PAYLOAD_EXPIRY] ?? null) && $payload[self::PAYLOAD_EXPIRY]) {
+            $payload[self::PAYLOAD_EXPIRY] = time() + $this->ttl;
         }
 
-        if (isset($payload['nbf']) && is_string($payload['nbf'])) {
-            $payload['nbf'] = strtotime($payload['nbf']);
+        if (is_string($payload[self::PAYLOAD_NOT_BEFORE] ?? null)) {
+            $payload[self::PAYLOAD_NOT_BEFORE] = strtotime($payload[self::PAYLOAD_NOT_BEFORE]);
         }
 
         return implode('.', array_values($this->compile($headers, $payload)));
     }
 
-    /**
-     * Extract Data from JWT object
-     * @param string $jwt
-     * @return array
-     */
-    public function extract(
-        string $jwt
-    ): array {
+    public function extract(string $jwt): array
+    {
         $tokenParts = explode('.', $jwt);
         $json_header = base64_decode($tokenParts[0]);
         $json_payload = base64_decode($tokenParts[1]);
@@ -69,68 +62,61 @@ class JWT
         $header = json_decode($json_header, true);
         $payload = json_decode($json_payload, true);
 
-        return compact('header', 'payload', 'signature');
+        return [
+            self::HEADER => $header,
+            self::PAYLOAD => $payload,
+            self::SIGNATURE => $signature,
+        ];
     }
 
-    /**
-     * Check if JWT token is valid
-     * @param string $jwt
-     * @return bool
-     */
-    public function validate(
-        string $jwt
-    ): bool {
+    public function validate(string $jwt): bool
+    {
         $extracted = $this->extract($jwt);
-        $header = $extracted['header'] ?? [];
-        $payload = $extracted['payload'] ?? [];
-        $signature = $extracted['signature'] ?? '';
+        $header = $extracted[self::HEADER] ?? [];
+        $payload = $extracted[self::PAYLOAD] ?? [];
+        $signature = $extracted[self::SIGNATURE] ?? '';
 
-        if (!empty(trim($this->issuer)) && (!isset($payload['iss']) || $this->issuer !== $payload['iss'])) {
+        $isIssuerSetInPayload = !isset($payload[self::PAYLOAD_ISSUER])
+            || $this->issuer !== $payload[self::PAYLOAD_ISSUER];
+        if (!empty(trim($this->issuer)) && $isIssuerSetInPayload) {
             return false;
         }
 
-        if(isset($payload['iat']) && $payload['iat'] - time() > 0) {
+        if($payload[self::PAYLOAD_ISSUED_AT] ?? 0 - time() > 0) {
             return false;
         }
 
-        if(isset($payload['exp']) && $payload['exp'] - time() < 0) {
+        if($payload[self::PAYLOAD_EXPIRY] ?? PHP_INT_MAX - time() < 0) {
             return false;
         }
 
-        if(isset($payload['nbf']) && $payload['nbf'] - time() > 0) {
+        if($payload[self::PAYLOAD_NOT_BEFORE] ?? 0 - time() > 0) {
             return false;
         }
 
-        return $signature === $this->compile($header, $payload)['signature'];
+        return $signature === $this->compile($header, $payload)[self::SIGNATURE];
     }
 
-    /**
-     * Internal function that builds a JWT
-     * @param array $headerData
-     * @param array $payloadData
-     * @return array
-     */
-    protected function compile(
-        array $headerData,
-        array $payloadData,
-    ): array {
-        $header = $this->base64UrlEncode(json_encode($headerData));
-        $payload = $this->base64UrlEncode(json_encode($payloadData));
+    protected function compile(array $headerData, array $payloadData): array
+    {
+        $header = StringHelper::base64UrlEncode(json_encode($headerData));
+        $payload = StringHelper::base64UrlEncode(json_encode($payloadData));
 
-        $signatureData = hash_hmac('SHA256', "$header.$payload", $this->secret, true);
-        $signature = $this->base64UrlEncode($signatureData);
+        $signatureData = hash_hmac(self::HASH_ALGORITHM, "$header.$payload", $this->secret, true);
+        $signature = StringHelper::base64UrlEncode($signatureData);
 
-        return compact('header', 'payload', 'signature');
+        return [
+            self::HEADER => $header,
+            self::PAYLOAD => $payload,
+            self::SIGNATURE => $signature,
+        ];
     }
+}
 
-    /**
-     * base64 url encode
-     * @param string $str
-     * @return string
-     */
-    protected function base64UrlEncode(
-        string $str
-    ): string {
+class StringHelper
+{
+    public static function base64UrlEncode(string $str): string
+    {
         return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
     }
 }
